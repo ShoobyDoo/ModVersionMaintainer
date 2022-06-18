6130a3d7553c9d0d7809989cd99db6d95d1d2afd
Shoaib Ali <coolbrampton@gmail.com>
2022-06-17 23:59:54 -0400

    separated web scraper to its own function + small changes
    - Separated web scraper into its own function and created a relatively consistent loop to ensure successful return codes
    - Small visual tweaks to output

--- mvm.py
+++ mvm.py
@@ -35,6 +35,7 @@
 import sqlite3
 import glob
 import re
+import time
 import cloudscraper
 
 
@@ -69,6 +70,8 @@ def main():
 
     # Print disclaimer message and ask user to accept or deny; continue is accept, exit if deny
     disclaimer = input(f"{disclaimer_msg}\n\n(Y)es/(N)o: ")
+    # TODO: remove this
+    disclaimer = 'y'
     if disclaimer in ['y', 'yes']:
         pass
     else:
@@ -76,7 +79,7 @@ def main():
         exit()
 
     # Prompt user for %appdata% roaming .minecraft/mods folder
-    yn_default = input(f"Default mods folder location: [{mods_folder}]\nConfirm? (Y)es/(N)o: ")
+    yn_default = input(f"\nDefault mods folder location: [{mods_folder}]\nConfirm? (Y)es/(N)o: ")
 
     # If its not in the default location, have the user provide the absolute path
     if yn_default.lower() in ["n", "no"]: mods_folder = input("\nMods folder not in default location, please provide the full path to your mods folder.\nPath: ")
@@ -122,7 +125,7 @@ def main():
         mods_dict.update({mod_name: jar_file})
 
         # Get index of current mod + 1 since zero indexed to get the mod in the list and print the mod and file name
-        print(f"{mods.index(mod) + 1}. {mod_name.ljust(31)} (/) {jar_file}")
+        print(f"{mods.index(mod) + 1}. {mod_name.ljust(33)} | {jar_file}")
 
     # Let user know how many total mods were parsed, then let user know that we're checking for the mod type
     # This will be later used to determine the filter that needs to be applied when scraping the webpage for download links
@@ -139,55 +142,73 @@ def main():
     # Now we let the user know that we need to build the mod links that correspond to the curse forge website
     print("\nAttempting to build mod links to CurseForge...")
     
-    # We initialize a CloudFare bypass scraper (works the exact same as requests library)
-    scraper = cloudscraper.create_scraper(delay=10)
+    process_links(mod_type)
 
-    # We loop through all the mods
-    for key in mods_dict:
-        # We try to determine if there's a mod name that got taken in but it's two words. for example MouseTweaks.
-        multi_word = re.findall('[A-Z][^A-Z]*', key)
-        
-        # We instantiate a variable to store the formatted mod name which will be used to pull the respective mod page
-        key_fmtd = key
 
-        # If multi_word was true, meaning there were perhaps two words (which has been split by the re.findall method)
-        if multi_word:
-            # We want to reset the formatted mod name variable so as to build it up ourselves
-            key_fmtd = ""
-            # We loop through each word that was found, for example ['Mouse', 'Tweaks']
-            for word in multi_word:
-                # if the word in multi is the last word, dont include the spacer dash
-                if word == multi_word[-1]:
-                    key_fmtd += word.lower()
-                # otherwise include it, most links use it. example: curseforge.com/minecraft/mc-mods/mouse-tweaks
-                else:
-                    key_fmtd += word.lower() + "-"
+def process_links(mod_type, delay=5) -> None:
+    global mods_dict
+    global known_aliases
 
-        # Now we want to see if there were any outliers, so we take the formatted mod name and try running it through any known aliases
-        # If there are no errors, it means that mod name had an alias which has been set to the formatted mod name, otherwise we continue
-        try: key_fmtd = known_aliases[key_fmtd]
-        except KeyError: pass
+    while True:
+        is_403 = False
 
-        # Now we just build the final mod url using all the previously processed bits and pieces and let the user know
-        mod_url = f"{curseforge_url}{key_fmtd}/files?sort={mod_types[mod_type]}"
-        print(f"Processing {key_fmtd}: ".ljust(35) + f"(?) {mod_url}")
+        # We initialize a CloudFare bypass scraper (works the exact same as requests library)
+        scraper = cloudscraper.create_scraper(delay=10)
 
-        # And here we begin scraping the webpages. We spoof the user agent as firefox and let cloudscraper hopefuly bypass cloudfare
-        # Please note that during my very limited testing (ie. like a few hours) it's kind of inconsistent. Sometimes the requests will go
-        # through, and sometimes we're met with a 403 error because of captcha. I'm trying to get an api key from curse forge to circumvent
-        # this issue, but that seems to be the case for now. 
-        # 
-        # Just please do not abuse the update checks; requesting too frequently and you will likely get captcha blocked.
-        # TODO: The actual request portion has been commented out while I work on the rest of functionality so as not to constantly send reqs.
+        # We loop through all the mods
+        for key in mods_dict:
+            # We try to determine if there's a mod name that got taken in but it's two words. for example MouseTweaks.
+            multi_word = re.findall('[A-Z][^A-Z]*', key)
+            
+            # We instantiate a variable to store the formatted mod name which will be used to pull the respective mod page
+            key_fmtd = key
 
-        # res = scraper.get(mod_url, headers={'User-Agent': 'Mozilla/5.0'})
+            # If multi_word was true, meaning there were perhaps two words (which has been split by the re.findall method)
+            if multi_word:
+                # We want to reset the formatted mod name variable so as to build it up ourselves
+                key_fmtd = ""
+                # We loop through each word that was found, for example ['Mouse', 'Tweaks']
+                for word in multi_word:
+                    # if the word in multi is the last word, dont include the spacer dash
+                    if word == multi_word[-1]:
+                        key_fmtd += word.lower()
+                    # otherwise include it, most links use it. example: curseforge.com/minecraft/mc-mods/mouse-tweaks
+                    else:
+                        key_fmtd += word.lower() + "-"
 
-        # if res.ok:
-        #     print("".ljust(39) + f"(✓) {res.status_code}: OK.")
-        # elif res.status_code == 404:
-        #     print("".ljust(39) + f"(?) {res.status_code}: NOT FOUND; CHECK URL MANUALLY.")
-        # else:
-        #     print("".ljust(39) + f"{res.status_code} (X) FORBIDDEN; LIKELY REQUEST IS CAPTCHA BLOCKED.")
+            # Now we want to see if there were any outliers, so we take the formatted mod name and try running it through any known aliases
+            # If there are no errors, it means that mod name had an alias which has been set to the formatted mod name, otherwise we continue
+            try: key_fmtd = known_aliases[key_fmtd]
+            except KeyError: pass
+
+            # Now we just build the final mod url using all the previously processed bits and pieces and let the user know
+            mod_url = f"{curseforge_url}{key_fmtd}/files?sort={mod_types[mod_type]}"
+            print(f"Processing {key_fmtd}: ".ljust(37) + f"| {mod_url}")
+
+            # And here we begin scraping the webpages. We spoof the user agent as firefox and let cloudscraper hopefuly bypass cloudfare
+            # Please note that during my very limited testing (ie. like a few hours) it's kind of inconsistent. Sometimes the requests will go
+            # through, and sometimes we're met with a 403 error because of captcha. I'm trying to get an api key from curse forge to circumvent
+            # this issue, but that seems to be the case for now. 
+            # 
+            # Just please do not abuse the update checks; requesting too frequently and you will likely get captcha blocked.
+            # TODO: The actual request portion has been commented out while I work on the rest of functionality so as not to constantly send reqs.
+
+            # res = scraper.get(mod_url, headers={'User-Agent': 'Mozilla/5.0'})
+            #
+            # if res.ok:
+            #     print("Response: ".ljust(37) + f"| {res.status_code}: OK.       (✓)\n")
+            # elif res.status_code == 404:
+            #     print("Response: ".ljust(37) + f"| {res.status_code}: NOT FOUND (?); CHECK URL MANUALLY.\n")
+            # else:
+            #     print("Response: ".ljust(37) + f"| {res.status_code}: FORBIDDEN (X); LIKELY REQUEST IS CAPTCHA BLOCKED.\n")
+            #     is_403 = True
+
+        if is_403:
+            print("\nSome if not all links returned Status code: 403 (forbidden). Waiting 3 seconds to retry...\n")
+            time.sleep(delay)
+            continue
+        else:
+            break
 
 
 if __name__ == '__main__':
