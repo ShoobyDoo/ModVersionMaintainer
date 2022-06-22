dbacbfcad04bba789ecba218255d181c797669ec
Shoaib Ali <coolbrampton@gmail.com>
2022-06-22 03:17:39 -0400

    minor bug fix to requirements.txt check
    - Added a pause to ensure the script doesn't just close immediately as its run.

--- run.bat
+++ run.bat
@@ -4,12 +4,13 @@ echo Mod Version Maintainer - Auto mode
 echo:
 echo Checking for Python virtual environment...
 
-if exist .venv\ (.venv\Scripts\activate.bat && python -m main)
+if exist .venv\ (.venv\Scripts\activate.bat && cls && python -m main)
 
 echo Virtual environment doesnt exist. Looking for requirements.txt...
 echo:
 if exist requirements.txt goto :REQUIREMENTS_EXISTS
 echo The requirements.txt file is needed to proceed. Please obtain it via the GitHub page!
+pause
 goto :EOF
 
 :REQUIREMENTS_EXISTS
